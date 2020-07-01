87bd4f4b5b80a91422a4e5512d357919815ccd99
debien <christopherdebien@gmail.com>
2020-07-01 10:10:09 +0200

    ajout fonction

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var url = "http://data.fixer.io/api/latest?access_key=479804b12673acc118f393d015570d8b&base=EUR&symbols=GBP,JPY,EUR,USD"
 
 
+function recupValeur(){
+    $.ajax({
+        url : url,
+        type : "GET",
+        success : function (res, status, req){
+            console.log("res:", res. rates.USD);
+            console.log("req:", req);
+        }//callback
+        
+         
+    })
+}
